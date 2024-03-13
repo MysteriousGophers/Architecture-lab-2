9e6eac04dc5b6ad3300bcb8d1f43a9440a7aa395
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-03-13 09:23:53 +0100

    Implement simple `PostfixToInfix` function with tests

--- implementation.go
+++ implementation.go
@@ -1,9 +1,55 @@
 package lab2
 
-import "fmt"
+import (
+	"strings"
+)
+
+type BinaryTreeNode struct {
+	Value string
+	Left  *BinaryTreeNode
+	Right *BinaryTreeNode
+}
+
+func ConstructBinaryTree(postfixExpr string) *BinaryTreeNode {
+	stack := make([]*BinaryTreeNode, 0)
+
+	tokens := strings.Fields(postfixExpr)
+
+	for _, token := range tokens {
+		node := &BinaryTreeNode{Value: token}
+
+		if isOperator(token) {
+			right := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			left := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			node.Left = left
+			node.Right = right
+		}
+		stack = append(stack, node)
+	}
+	return stack[0]
+}
+
+func isOperator(token string) bool {
+	operators := map[string]bool{"+": true, "-": true, "*": true, "/": true}
+	return operators[token]
+}
+
+func InorderTraversal(root *BinaryTreeNode) string {
+	if root != nil {
+		left := InorderTraversal(root.Left)
+		right := InorderTraversal(root.Right)
+		return left + root.Value + " " + right
+	}
+	return ""
+}
 
 // TODO: document this function.
 // PostfixToInfix converts
-func PostfixToInfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func PostfixToInfix(expression string) (string, error) {
+	root := ConstructBinaryTree(expression)
+	result := InorderTraversal(root)
+	return strings.TrimSpace(result), nil
 }
--- implementation_test.go
+++ implementation_test.go
@@ -2,17 +2,48 @@ package lab2
 
 import (
 	"fmt"
+	. "gopkg.in/check.v1"
 	"testing"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	PostfixToInfix("+ 5 * - 4 2 3")
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+func (s *MySuite) TestBob(c *C) {
+	c.Assert("bob", Equals, "bob")
+}
+
+func (s *MySuite) TestPostfixToInfixSimpleAddition(c *C) {
+	result, err := PostfixToInfix("254 256 +")
+	c.Assert(err, IsNil)
+	c.Assert(result, Equals, "254 + 256")
+}
+
+func (s *MySuite) TestPostfixToInfixSimpleSubtraction(c *C) {
+	result, err := PostfixToInfix("4 1 -")
+	c.Assert(err, IsNil)
+	c.Assert(result, Equals, "4 - 1")
+}
+
+func (s *MySuite) TestPostfixToInfixSimpleMultiplication(c *C) {
+	result, err := PostfixToInfix("4 5 *")
+	c.Assert(err, IsNil)
+	c.Assert(result, Equals, "4 * 5")
+}
+
+func (s *MySuite) TestPostfixToInfixSimpleDivision(c *C) {
+	result, err := PostfixToInfix("4 2 /")
+	c.Assert(err, IsNil)
+	c.Assert(result, Equals, "4 / 2")
 }
 
 func ExamplePrefixToPostfix() {
-	res, _ := PostfixToInfix("+ 2 2")
+	res, _ := PostfixToInfix("2 2 +")
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// 2 + 2
 }
