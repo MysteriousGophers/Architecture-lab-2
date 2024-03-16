f47c664cb5af28b060124a632992d83288058d9a
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-03-16 10:30:24 +0200

    feature: implemented compute handler

--- handler.go
+++ handler.go
@@ -1,12 +1,28 @@
 package lab2
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	Input  io.Reader
+	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
-	return nil
+	reader := bufio.NewReader(ch.Input)
+	input, _ := reader.ReadString('\n')
+	formattedInput := strings.TrimRight(input, "\n")
+	result, err := PostfixToInfix(formattedInput)
+	if err != nil {
+		fmt.Fprintln(ch.Output, err)
+	} else {
+		fmt.Fprintln(ch.Output, result)
+	}
+	return err
 }
