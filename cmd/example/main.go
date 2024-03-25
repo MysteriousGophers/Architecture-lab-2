7cdbefbebc33e9910c36a472cdee01bdacff95bb
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2024-03-26 01:32:21 +0200

    Add closure of output file after write

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -22,11 +22,18 @@ func main() {
 	flag.StringVar(&outputFile, "o", "", "File to write to")
 	flag.Parse()
 
-	reader, writer, err := getReaderAndWriter()
+	reader, writer, closer, err := getReaderAndWriter()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error: ", err)
 		os.Exit(1)
 	}
+	defer func() {
+		if closer != nil {
+			if err := closer(); err != nil {
+				fmt.Fprintln(os.Stderr, "Error closing file: ", err)
+			}
+		}
+	}()
 	handler := &lab2.ComputeHandler{Reader: reader, Writer: writer}
 	err = handler.Compute()
 	if err != nil {
@@ -35,18 +42,19 @@ func main() {
 	}
 }
 
-func getReaderAndWriter() (io.Reader, io.Writer, error) {
+func getReaderAndWriter() (io.Reader, io.Writer, func() error, error) {
 	if inputExpression == "" && inputFile == "" {
-		return nil, nil, errors.New("no provided expression")
+		return nil, nil, nil, errors.New("no provided expression")
 	} else if inputExpression != "" && inputFile != "" {
-		return nil, nil, errors.New("provided expression in both ways")
+		return nil, nil, nil, errors.New("provided expression in both ways")
 	}
 	var reader io.Reader
 	var writer io.Writer
+	var closer func() error
 	if inputFile != "" {
 		file, err := os.Open(inputFile)
 		if err != nil {
-			return nil, nil, errors.New("error reading file")
+			return nil, nil, nil, errors.New("error reading file")
 		}
 		reader = file
 	} else {
@@ -55,11 +63,15 @@ func getReaderAndWriter() (io.Reader, io.Writer, error) {
 	if outputFile != "" {
 		file, err := os.OpenFile(outputFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
-			return nil, nil, errors.New("error opening file to write output to")
+			return nil, nil, nil, errors.New("error opening file to write output to")
 		}
 		writer = file
+		closer = func() error {
+			return file.Close()
+		}
 	} else {
 		writer = os.Stdout
+		closer = nil
 	}
-	return reader, writer, nil
+	return reader, writer, closer, nil
 }
